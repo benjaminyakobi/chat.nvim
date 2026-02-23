fc7dce047a9213745cff90d7f5847dd4de2d45d8
benjamin <benjaminyakobi@gmail.com>
2026-02-23 17:02:29 +0200

    parsed lua file with the proper editorconfig

--- lua/chat.lua
+++ lua/chat.lua
@@ -1,15 +1,15 @@
 local M = {}
 
 function M.setup(opts)
-	opts = opts or {}
+  opts = opts or {}
 
-	vim.keymap.set("n", "<Leader>h", function()
-		if opts.name then
-			print("chat: hello, " .. opts.name)
-		else
-			print("hello")
-		end
-	end)
+  vim.keymap.set("n", "<Leader>h", function()
+    if opts.name then
+      print("chat: hello, " .. opts.name)
+    else
+      print("hello")
+    end
+  end)
 end
 
 return M
