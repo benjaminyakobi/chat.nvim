28f9c6f91752a672d3f7d772842073e091baba28
benjamin <benjaminyakobi@gmail.com>
2026-02-23 17:20:42 +0200

    renamed key binding to test reloading

--- lua/chat.lua
+++ lua/chat.lua
@@ -3,11 +3,11 @@ local M = {}
 function M.setup(opts)
   opts = opts or {}
 
-  vim.keymap.set("n", "<Leader>h", function()
+  vim.keymap.set("n", "<Leader>a", function()
     if opts.name then
       print("chat: hello, " .. opts.name)
     else
-      print("hello")
+      print("chat: hello")
     end
   end)
 end
